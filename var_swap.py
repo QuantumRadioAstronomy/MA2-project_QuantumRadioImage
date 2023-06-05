fb20fac6fbe0e6ee87aa5b04415574ba834b8d1e
thmaoe <115552079+thmaoe@users.noreply.github.com>
2023-06-05 12:38:49 +0200

    add calib with swap

---
+++ var_swap.py
@@ -0,0 +1,93 @@
+import numpy as np
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, Aer, execute, transpile
+import qoie as qpie
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+import time
+
+def swap_test(state1, state2):
+    
+    anc = QuantumRegister(1,name='ancilla')
+    meas = ClassicalRegister(1)
+    state_1 = QuantumRegister(state1.num_qubits,name='state1')
+    state_2 = QuantumRegister(state2.num_qubits,name='state2')
+
+    qc = QuantumCircuit(anc,state_1,state_2,meas)
+
+    qc.h(anc)
+    qc.append(state1,state_1)
+    qc.append(state2,state_2)
+
+    for i in range(1,state1.num_qubits+1):
+        qc.cswap(0,i,i+state1.num_qubits)
+    
+    qc.h(anc)
+    qc.measure(anc,meas)
+
+    return qc
+
+class swap_calib():
+
+    def __init__(self, V_ij, V_ijtilda, learn_param = 0.01, nloops = 1000, shift = np.pi/2):
+
+        self.V_ij = V_ij
+        self.V_ijtilda = V_ijtilda
+        self.learn = learn_param
+        self.nloops = nloops
+        self.shift = shift
+        self.cost = []
+        self.steps = []
+        self.V_ijtilda_qc = qpie.qpie_circuit(V_ijtilda)
+    
+    def cost_function(self,params):
+
+        G_ = np.outer(params,params.T)
+        V_ij_ = G_ * self.V_ij
+
+        state1 = qpie.qpie_circuit(V_ij_)
+        qc = swap_test(state1,self.V_ijtilda_qc)
+
+        return qc
+
+        # backend = Aer.get_backend('qasm_simulator')
+        # results = execute(qc, backend=backend, shots=1024).result()
+        # answer = results.get_counts()
+
+        # return answer.get('1',0)/1024, qc
+    
+    def gradient_function(self,params):
+
+        gradient = np.zeros(params.shape[0])
+        shift = self.shift
+
+        for i in range(params.shape[0]):
+            shift_p = params.copy()
+            shift_p[i] += shift
+
+            shift_m = params.copy()
+            shift_m[i] -= shift
+
+            cost_p = self.cost_function(shift_p)
+            cost_m = self.cost_function(shift_m)
+
+            gradient[i] = 1/2 * (cost_p - cost_m)
+
+        return gradient
+    
+    def grad_desc(self,params):
+
+        for i in range(self.nloops):
+            params = params - self.learn * self.gradient_function(params)
+            self.cost.append(self.cost_function(params))
+            self.steps.append(i)
+
+        return params/np.linalg.norm(params)
+    
+    def class_opti(self, params):
+
+        res = minimize(self.cost_function, params, method='COBYLA', tol=1e-9)
+        
+        return res.x/np.linalg.norm(res.x), res.nfev
+
+
+
