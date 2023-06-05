80002788f4640102de154256eee943c044d3ec1b
thmaoe <115552079+thmaoe@users.noreply.github.com>
2023-06-05 12:58:44 +0200

    added comments to calib code

--- var_swap.py
+++ var_swap.py
@@ -5,16 +5,17 @@ import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 import time
 
-def swap_test(state1, state2):
+def swap_test(state1, state2): #Function to perform swap test between 2 given states
     
-    anc = QuantumRegister(1,name='ancilla')
-    meas = ClassicalRegister(1)
-    state_1 = QuantumRegister(state1.num_qubits,name='state1')
-    state_2 = QuantumRegister(state2.num_qubits,name='state2')
+    anc = QuantumRegister(1,name='ancilla') #ancilla qubit 
+    meas = ClassicalRegister(1) #measurement
+    state_1 = QuantumRegister(state1.num_qubits,name='state1') #register for state 1
+    state_2 = QuantumRegister(state2.num_qubits,name='state2') #and state 2
 
-    qc = QuantumCircuit(anc,state_1,state_2,meas)
+    qc = QuantumCircuit(anc,state_1,state_2,meas) #define the quantum circuit
 
-    qc.h(anc)
+    #Then we just apply SWAP test : H on ancilla, load the states, SWAPS and H on ancilla
+    qc.h(anc) 
     qc.append(state1,state_1)
     qc.append(state2,state_2)
 
@@ -22,7 +23,7 @@ def swap_test(state1, state2):
         qc.cswap(0,i,i+state1.num_qubits)
     
     qc.h(anc)
-    qc.measure(anc,meas)
+    qc.measure(anc,meas) #meaasure ancilla
 
     return qc
 
@@ -39,23 +40,21 @@ class swap_calib():
         self.steps = []
         self.V_ijtilda_qc = qpie.qpie_circuit(V_ijtilda)
     
-    def cost_function(self,params):
+    def cost_function(self,params): #cost function : return proba of ancilla being in |1>.
 
         G_ = np.outer(params,params.T)
-        V_ij_ = G_ * self.V_ij
+        V_ij_ = G_ * self.V_ij #compute with actual parameters
 
         state1 = qpie.qpie_circuit(V_ij_)
-        qc = swap_test(state1,self.V_ijtilda_qc)
+        qc = swap_test(state1,self.V_ijtilda_qc) 
 
-        return qc
+        backend = Aer.get_backend('qasm_simulator')
+        results = execute(qc, backend=backend, shots=1024).result()
+        answer = results.get_counts() #do simulation
 
-        # backend = Aer.get_backend('qasm_simulator')
-        # results = execute(qc, backend=backend, shots=1024).result()
-        # answer = results.get_counts()
-
-        # return answer.get('1',0)/1024, qc
+        return answer.get('1',0)/1024 #return proba
     
-    def gradient_function(self,params):
+    def gradient_function(self,params): #return the gradients, computed with shift rule
 
         gradient = np.zeros(params.shape[0])
         shift = self.shift
@@ -74,7 +73,7 @@ class swap_calib():
 
         return gradient
     
-    def grad_desc(self,params):
+    def grad_desc(self,params): #loop for gradient descent
 
         for i in range(self.nloops):
             params = params - self.learn * self.gradient_function(params)
@@ -83,7 +82,7 @@ class swap_calib():
 
         return params/np.linalg.norm(params)
     
-    def class_opti(self, params):
+    def class_opti(self, params): #classical optimizer
 
         res = minimize(self.cost_function, params, method='COBYLA', tol=1e-9)
         
