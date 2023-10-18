d0e35e022481e79d7cf12db2115b5ed546953a5e
thmaoe <115552079+thmaoe@users.noreply.github.com>
2023-10-18 18:01:49 +0200

    main clean

--- main_try.py
+++ main_try.py
@@ -48,60 +48,60 @@ for i in range(1): #number of experience you want
     cost_g.append(var.cost_function(g)) #and for good g
 
     clas,nit = var.class_opti(parameters) #optimize with classical
-    # gd = var.grad_desc(parameters) #and with QGD
+    gd = var.grad_desc(parameters) #and with QGD
 
-    #We add to observations the results
+    # We add to observations the results
 
-    # opti_class.append(clas)
-    # opti_gd.append(gd)
-    # it_clas.append(nit)
+    opti_class.append(clas)
+    opti_gd.append(gd)
+    it_clas.append(nit)
 
-    # cost_clas.append(var.cost_function(clas))
-    # cost_gd.append(var.cost_function(gd))
+    cost_clas.append(var.cost_function(clas))
+    cost_gd.append(var.cost_function(gd))
 
-    # diff_class.append(np.abs(clas-g))
-    # diff_gd.append(np.abs(gd-g))
+    diff_class.append(np.abs(clas-g))
+    diff_gd.append(np.abs(gd-g))
 
-    # his_clas.append(np.abs(clas[0]-g[0]))
-    # his_clas.append(np.abs(clas[1]-g[1]))
-    # his_q.append(np.abs(gd[0]-g[0]))
-    # his_q.append(np.abs(gd[1]-g[1]))
+    his_clas.append(np.abs(clas[0]-g[0]))
+    his_clas.append(np.abs(clas[1]-g[1]))
+    his_q.append(np.abs(gd[0]-g[0]))
+    his_q.append(np.abs(gd[1]-g[1]))
 
-    # plt.figure() #plot cost/step for QGD
-    # plt.xlabel('Step',fontsize=15)
-    # plt.ylabel('Cost Function', fontsize=15)
-    # plt.plot(var.steps,var.cost)
+    plt.figure() #plot cost/step for QGD
+    plt.xlabel('Step',fontsize=15)
+    plt.ylabel('Cost Function', fontsize=15)
+    plt.plot(var.steps,var.cost)
 
 
 #Prints observations
-# print(inits)
-# print(opti_class)
-# print(opti_gd)
-# print(good_g)
-# print(diff_class)
-# print(diff_gd)
-# print(it_clas)
-# print(cost_init)
-# print(cost_clas)
-# print(cost_gd)
-# print(cost_g)
+print(inits)
+print(opti_class)
+print(opti_gd)
+print(good_g)
+print(diff_class)
+print(diff_gd)
+print(it_clas)
+print(cost_init)
+print(cost_clas)
+print(cost_gd)
+print(cost_g)
 
-# print(his_clas)
-# print(his_q)
+print(his_clas)
+print(his_q)
 
-# counts, bins = np.histogram(his_clas)
-# plt.figure()
+counts, bins = np.histogram(his_clas)
+plt.figure()
 
-# plt.xlabel('% Absolute Error')
-# plt.ylabel('Counts')
+plt.xlabel('% Absolute Error')
+plt.ylabel('Counts')
 
-# plt.hist(his_clas, bins = bins, color = 'blue', alpha = 0.5, histtype = 'stepfilled')
-# plt.hist(his_clas, bins = bins, edgecolor = 'blue', alpha = 1.0, histtype = 'step', label='Hybrid')
+plt.hist(his_clas, bins = bins, color = 'blue', alpha = 0.5, histtype = 'stepfilled')
+plt.hist(his_clas, bins = bins, edgecolor = 'blue', alpha = 1.0, histtype = 'step', label='Hybrid')
 
-# # counts, bins = np.histogram(his_q)
-# plt.hist(his_q, bins = bins, color = 'red', alpha = 0.3, histtype = 'stepfilled')
-# plt.hist(his_q, bins = bins, edgecolor = 'red', alpha = 1.0, histtype = 'step', label = 'QGD')
+# counts, bins = np.histogram(his_q)
+plt.hist(his_q, bins = bins, color = 'red', alpha = 0.3, histtype = 'stepfilled')
+plt.hist(his_q, bins = bins, edgecolor = 'red', alpha = 1.0, histtype = 'step', label = 'QGD')
 
-# plt.legend()
+plt.legend()
 
-# plt.show()
+plt.show()
